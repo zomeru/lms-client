280a3e3d49b465539ee3be32cd50335ec5b54198
zomeru <zomerzxc.19@gmail.com>
2022-05-06 02:33:42 +0800

    Setup book section in home page

---
+++ src/components/_page/Home/HomeBooks/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledBookContainer = styled.div`
+  padding: 70px var(--h-padding-md);
+
+  .header-title {
+    text-align: center;
+    font-size: 32px;
+  }
+
+  .top-book-container {
+    margin-bottom: 80px;
+  }
+
+  .new-book-container {
+  }
+`;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Layout } from './Layout';
 export { default as Navbar } from './Navbar';
 export { default as Section } from './Section';
 export { default as NextSeoHead } from './NextSeoHead';
+export { default as BookCard } from './BookCard';
